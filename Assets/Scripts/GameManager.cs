fd67681f3f7083d10d5d17b4cfbe352982b5d3a4
Clea <clea_h@p-hall.net>
2024-05-22 14:56:17 -0700

    Card Shuffle, draw, discard

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>();
+    public List<Card> discardPile = new List<Card>();
+    public Transform[] cardSlots;
+    public bool[] availableCardSlots;
+    public TMP_Text deckSizeText;
+    public TMP_Text discardPileText;
+
+
+    public void DrawCard(){
+        if(deck.Count >=1){
+            Card randCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++){
+                if(availableCardSlots[i]== true){
+                    randCard.gameObject.SetActive(true);
+                    randCard.handIndex = i;
+                    
+                    randCard.transform.position = cardSlots[i].position;
+                    randCard.hasBeenPlayed = false;
+
+                    availableCardSlots[i] = false;
+                    deck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+    }
+
+    public void Shuffle(){
+        if(discardPile.Count >= 1){
+            foreach(Card card in discardPile){
+                deck.Add(card);
+            }
+            discardPile.Clear();
+        }
+    }
+
+    private void Update(){
+        deckSizeText.text = deck.Count.ToString();
+        discardPileText.text = discardPile.Count.ToString();
+    }
+}
