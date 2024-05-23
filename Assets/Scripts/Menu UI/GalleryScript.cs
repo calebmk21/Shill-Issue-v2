aa390113d72a7eb65813099c814777b4e38ee400
Caleb Kim <calebmk21@gmail.com>
2024-05-22 23:14:17 -0700

    gallery framework

---
+++ Assets/Scripts/Menu UI/GalleryScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ShillIssue;
+
+public class GalleryScript : MonoBehaviour
+{
+    [Header("Panels")] 
+    public GameObject baseMenu;
+    public GameObject galleryTab;
+    public GameObject settingsTab;
+    public GameObject creditsTab;
+
+    [Header("Test")] 
+    [SerializeField] public bool testBool;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
