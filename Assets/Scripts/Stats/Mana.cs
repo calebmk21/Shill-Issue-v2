06c7dc6d35b018425acd2707c8bfeae81349da46
Laura Ness <lness@ucla.edu>
2024-05-22 22:16:22 -0700

    Health + Mana & Health Bar + Mana Bar
    will wait for game logic code to figure out any sort of stats controller

---
+++ Assets/Scripts/Stats/Mana.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mana : MonoBehaviour
+{
+    /*
+        General purpose mana.  
+    */
+    public int maxMana = 100; 
+    public int currentMana; 
+    public ManaBar manaBar; 
+
+    void Start()
+    {
+        currentMana = maxMana; 
+        manaBar.SetMaxMana(maxMana); 
+    }
+
+    public void DecreaseMana(int dec) {
+        currentMana -= dec; 
+
+        if (currentMana < 0) {
+            currentMana = 0; 
+        }
+
+        // set health bar
+        manaBar.SetMana(currentMana);
+    }
+
+    public void IncreaseMana(int inc) {
+        currentMana += inc; 
+
+        if (currentMana > 100) {
+            currentMana = 100; 
+        }
+        // set health bar
+        manaBar.SetMana(currentMana);
+    }
+
+    public int GetMana() {
+        return currentMana; 
+    }
+}
