5a071d4129543074ffc450255d8a354107853938
michaelkho <155688621+mkhojastegan@users.noreply.github.com>
2024-05-25 16:00:11 -0700

    A better deck manager :o

--- Assets/Scripts/CardCode/DeckManager.cs
+++ Assets/Scripts/CardCode/DeckManager.cs
@@ -1,29 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using ShillIssue;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class DeckManager : MonoBehaviour
 {
     public List<Card> allCards = new List<Card>();
+    public int startingHandSize;
 
-    public int startingHandSize = 0;
+    private HandManager handManager;
 
     private int currentIndex = 0;
     public int maxHandSize;
+    // Currently 2, but you can change it however you want
     public int currentHandSize;
-    private HandManager handManager;
 
     void Start()
     {
         //Load all card assets from the Resources folder
         Card[] cards = Resources.LoadAll<Card>("Cards");
+        Debug.Log(cards.Length);        
 
         //Add the loaded cards to the allCards list
         allCards.AddRange(cards);
 
         handManager = FindObjectOfType<HandManager>();
+
+        // Set values derp
         maxHandSize = handManager.maxHandSize;
+        startingHandSize = 2;
+        currentHandSize = startingHandSize;
+        if(startingHandSize > maxHandSize){
+            Debug.Log("Starting hand size is too large");
+        }
         for (int i = 0; i < startingHandSize; i++)
         {
             Debug.Log($"Drawing Card");
@@ -46,9 +58,14 @@ public class DeckManager : MonoBehaviour
 
         if (currentHandSize < maxHandSize)
         {
+            Debug.Log("Drawing card");
+            // Get random card
+            currentIndex = UnityEngine.Random.Range(0, allCards.Count);
             Card nextCard = allCards[currentIndex];
+
+            // Add card
             handManager.AddCardToHand(nextCard);
-            currentIndex = (currentIndex + 1) % allCards.Count;
+            currentHandSize++;
         }
     }
 }
