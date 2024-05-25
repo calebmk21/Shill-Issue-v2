b933407b33f3745bdaa46c851f8e8f12ffbaeec7
Clea <clea_h@p-hall.net>
2024-05-25 14:38:12 -0700

    Updated GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
-    private static GameManager _instance;
-    public static GameManager Instance { get { return _instance; } }
+ {
+    public static GameManager _instance { get; private set;}
+    public OptionsManager OptionsManager { get; private set; }
+    public AudioManager AudioManager{ get; private set; }
+    public DeckManager DeckManager{ get; private set; }
+
+    private int playerHealth;
+    private int difficulty = 5;
+
+    [SerializeField]
+    private List<CardEnumMatch> cardDictSetup = new List<CardEnumMatch>();
+    public Dictionary<CardEnum, ShillIssue.Card> entityObjectDict = new Dictionary<CardEnum, ShillIssue.Card>();
+
 
     public enum CardEnum
     {
         Card1, Card2
     }
-
-    [SerializeField]
-    private List<CardEnumMatch> cardDictSetup = new List<CardEnumMatch>();
-    public Dictionary<CardEnum, ShillIssue.Card> entityObjectDict = new Dictionary<CardEnum, ShillIssue.Card>();
 
     void Awake()
     {
@@ -26,11 +33,60 @@ public class GameManager : MonoBehaviour
         {
             _instance = this;
             DontDestroyOnLoad(_instance);
+            InitializeManagers();
         }
 
         foreach (CardEnumMatch match in cardDictSetup)
         {
             entityObjectDict.Add(match.cardType, match.cardData);
         }
+    }
+
+    private void InitializeManagers()
+    {
+        OptionsManager = GetComponentInChildren<OptionsManager>();
+        AudioManager = GetComponentInChildren<AudioManager>();
+        DeckManager = GetComponentInChildren<DeckManager>();
+
+        if( OptionsManager == null){
+            GameObject perfab = Resources.Load<GameObject>("Perbabs/OptionsManager");
+            if (perfab == null){
+                Debug.Log($"OptionsManager perfab not found");
+            } else {
+                Instantiate(perfab, transform.position, Quaternion.identity, transform);
+                OptionsManager = GetComponentInChildren<OptionsManager>();
+            }
+        }
+        if( AudioManager == null){
+            GameObject perfab = Resources.Load<GameObject>("Perbabs/AudioManager");
+            if (perfab == null){
+                Debug.Log($"AudioManager perfab not found");
+            } else {
+                Instantiate(perfab, transform.position, Quaternion.identity, transform);
+                AudioManager = GetComponentInChildren<AudioManager>();
+            }
+        }
+        if( DeckManager == null){
+            GameObject perfab = Resources.Load<GameObject>("Perbabs/DeckManager");
+            if (perfab == null){
+                Debug.Log($"DeckManager perfab not found");
+            } else {
+                Instantiate(perfab, transform.position, Quaternion.identity, transform);
+                DeckManager = GetComponentInChildren<DeckManager>();
+            }
+        }
+
+    }
+
+    public int PlayerHealth
+    {
+        get{ return playerHealth; }
+        set {playerHealth = value; }
+    }
+
+    public int Difficulty
+    {
+        get{ return difficulty; }
+        set { difficulty = value; }
     }
 }
